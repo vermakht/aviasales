93721067d2659ceea761d28074f17cee5157ccd0
Vera Makhonya <veramakhonya@mail.ru>
2023-11-16 19:52:47 +0300

    fix: форматирование кода

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import './index.css';
 import { App } from './components/app';
 import { store } from './store/global-store';
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
